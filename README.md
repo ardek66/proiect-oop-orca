f36f24f5a6897ea08fbf873bf26afb6c610545b8
IDF <ardek66@tutanota.com>
2024-03-23 23:00:12 +0200

    Implement Orca main class and split functionality into classes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Nu primesc notă pentru că nu am pus titlu și descriere
+# Orca-cpp
+Implementare a limbajului de programare ezoteric Orca.
 
 ### Important!
 Aveți voie cu cod generat de modele de limbaj la care nu ați contribuit semnificativ doar în folder-ul `generated`.
@@ -14,26 +15,26 @@ O cerință nu se consideră îndeplinită dacă este realizată doar ca o serie
 
 ### Tema 0
 
-- [ ] Nume proiect (poate fi schimbat ulterior)
-- [ ] Scurtă descriere a temei alese, ce v-ați propus să implementați
+- [x] Nume proiect (poate fi schimbat ulterior)
+- [x] Scurtă descriere a temei alese, ce v-ați propus să implementați
 
 ## Tema 1
 
 #### Cerințe
-- [ ] definirea a minim **3-4 clase** folosind compunere cu clasele definite de voi
-- [ ] constructori de inițializare
-- [ ] pentru o aceeași (singură) clasă: constructor de copiere, `operator=` de copiere, destructor
+- [x] definirea a minim **3-4 clase** folosind compunere cu clasele definite de voi
+- [x] constructori de inițializare
+- [x] pentru o aceeași (singură) clasă: constructor de copiere, `operator=` de copiere, destructor
 <!-- - [ ] pentru o altă clasă: constructor de mutare, `operator=` de mutare, destructor -->
 <!-- - [ ] pentru o altă clasă: toate cele 5 funcții membru speciale -->
-- [ ] `operator<<` pentru toate clasele pentru afișare (std::ostream)
-- [ ] cât mai multe `const` (unde este cazul)
-- [ ] implementarea a minim 3 funcții membru publice pentru funcționalități specifice temei alese, dintre care cel puțin 1-2 funcții mai complexe
+- [x] `operator<<` pentru toate clasele pentru afișare (std::ostream)
+- [x] cât mai multe `const` (unde este cazul)
+- [x] implementarea a minim 3 funcții membru publice pentru funcționalități specifice temei alese, dintre care cel puțin 1-2 funcții mai complexe
   - nu doar citiri/afișări sau adăugat/șters elemente într-un/dintr-un vector
-- [ ] scenariu de utilizare a claselor definite:
+- [x] scenariu de utilizare a claselor definite:
   - crearea de obiecte și apelarea tuturor funcțiilor membru publice în main
   - vor fi adăugate în fișierul `tastatura.txt` DOAR exemple de date de intrare de la tastatură (dacă există); dacă aveți nevoie de date din fișiere, creați alte fișiere separat
-- [ ] tag de `git`: de exemplu `v0.1`
-- [ ] serviciu de integrare continuă (CI); exemplu: GitHub Actions
+- [x] tag de `git`: de exemplu `v0.1`
+- [x] serviciu de integrare continuă (CI); exemplu: GitHub Actions
 
 ## Tema 2
 
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <tuple>
 
 #include <SFML/Graphics.hpp>
+#include <utility>
 
 #include <Helper.h>
 
@@ -15,7 +16,7 @@
 #include "env_fixes.h"                                              //
 //////////////////////////////////////////////////////////////////////
 
-#define TILE_SIZE 24.0f
+#define TILE_SIZE 24u
 //////////////////////////////////////////////////////////////////////
 /// This class is used to test that the memory leak checks work as expected even when using a GUI
 class SomeClass {
@@ -70,8 +71,27 @@ std::ostream &operator<<(std::ostream &os, const Rectangle &rt) {
     return os;
 }
 
+class ResourcesStore {
+private:
+    sf::Font _font;
+public:
+    bool loadResources() {
+        if (!_font.loadFromFile("TerminusTTF.ttf")) {
+            std::cerr << "Could not load font!" << std::endl;
+                return false;
+        }
+
+        return true;
+    }
+
+    sf::Font& getFont() {
+        return _font;
+    }
+};
+
 class Cursor : public sf::Drawable {
 private:
+    ResourcesStore _rs;
     sf::Text _curs_text;
     sf::RectangleShape _sel_shape;
     Rectangle _rt;
@@ -95,18 +115,90 @@ public:
         this->_sel_shape.setPosition(x1, y1);
         this->_sel_shape.setSize(sf::Vector2f(x2, y2) - sf::Vector2f(x1, y1));
     }
-    Cursor(const sf::Font& font, int x, int y) : _rt(x, y, 1, 1) {
+    Cursor(ResourcesStore rs, int x, int y) : _rs(std::move(rs)), _rt(x, y, 1, 1) {
         this->_curs_text.setCharacterSize(TILE_SIZE);
         this->_curs_text.setFillColor(sf::Color(0, 255, 0));
         this->_curs_text.setString("@");
-        this->_curs_text.setFont(font);
+        this->_curs_text.setFont(_rs.getFont());
         this->_curs_text.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Cursor& curs);
+};
+
+std::ostream &operator<<(std::ostream &os, const Cursor &curs) {
+    os << "Cursor: " << curs._rt;
+    return os;
+}
+
+class Orca {
+private:
+    ResourcesStore _rs;
+    Cursor _curs;
+    sf::RenderWindow _window;
+    int _w, _h;
+public:
+    friend std::ostream& operator<<(std::ostream& os, const Orca& orca);
+
+    Orca(ResourcesStore rs, int w, int h) : _rs(std::move(rs)), _curs(Cursor(_rs, 8, 8)), _w(w), _h(h) {
+        _window.create(sf::VideoMode({700, 800}), "My Window", sf::Style::Default);
+        _window.setVerticalSyncEnabled(true);
+    }
+
+    void mainLoop() {
+        while(_window.isOpen()) {
+            bool shouldExit = false;
+            sf::Event e{};
+
+            int offX = 0, offY = 0, offW = 0, offH = 0;
+
+            while(_window.pollEvent(e)) {
+                switch(e.type) {
+                    case sf::Event::Closed:
+                        _window.close();
+                        break;
+                    case sf::Event::Resized:
+                        std::cout << "New width: " << _window.getSize().x << '\n'
+                                  << "New height: " << _window.getSize().y << '\n';
+                        break;
+                    case sf::Event::KeyPressed:
+                        std::cout << _curs << std::endl;
+
+                        std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
+                        if(e.key.code == sf::Keyboard::Up) offY = -1;
+                        if(e.key.code == sf::Keyboard::Down) offY = 1;
+                        if(e.key.code == sf::Keyboard::Left) offX = -1;
+                        if(e.key.code == sf::Keyboard::Right) offX = 1;
+                        if(e.key.code == sf::Keyboard::W) offH = -1;
+                        if(e.key.code == sf::Keyboard::S) offH = 1;
+                        if(e.key.code == sf::Keyboard::A) offW = -1;
+                        if(e.key.code == sf::Keyboard::D) offW = 1;
+                        if(e.key.code == sf::Keyboard::Escape)
+                            shouldExit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if(shouldExit) {
+                _window.close();
+                break;
+            }
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(66ms);
+
+            _curs.update(offX, offY, offW, offH);
+            _window.clear();
+            _window.draw(_curs);
+            _window.display();
+        }
     }
 };
 
-class Orca {
+std::ostream &operator<<(std::ostream &os, const Orca &orca) {
+    os << "Orca(" << orca._w << ", " << orca._h << ")";
+    return os;
+}
 
-};
 int main() {
     ////////////////////////////////////////////////////////////////////////
     /// NOTE: this function call is needed for environment-specific fixes //
@@ -131,70 +223,13 @@ int main() {
     std::cout << rt1 << std::endl;
     std::cout << rt2 << std::endl;
 
-    sf::Font font;
-    if (!font.loadFromFile("TerminusTTF.ttf")) {
-        std::cerr << "Could not load font!" << std::endl;
+    ResourcesStore rs = ResourcesStore();
+    if(!rs.loadResources()){
         return 1;
     }
 
-    Cursor cursor = Cursor(font, 8, 8);
+    Orca orca = Orca(rs, 80, 80);
+    orca.mainLoop();
 
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                            ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    while(window.isOpen()) {
-        bool shouldExit = false;
-        sf::Event e{};
-
-        int offX = 0, offY = 0, offW = 0, offH = 0;
-
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Up) offY = -1;
-                if(e.key.code == sf::Keyboard::Down) offY = 1;
-                if(e.key.code == sf::Keyboard::Left) offX = -1;
-                if(e.key.code == sf::Keyboard::Right) offX = 1;
-                if(e.key.code == sf::Keyboard::W) offH = -1;
-                if(e.key.code == sf::Keyboard::S) offH = 1;
-                if(e.key.code == sf::Keyboard::A) offW = -1;
-                if(e.key.code == sf::Keyboard::D) offW = 1;
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
-                break;
-            }
-        }
-        if(shouldExit) {
-            window.close();
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(66ms);
-
-        cursor.update(offX, offY, offW, offH);
-        window.clear();
-        window.draw(cursor);
-        window.display();
-    }
     return 0;
 }
